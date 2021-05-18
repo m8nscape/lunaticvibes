a3a0acdd4496e5a99ba5c2cd80baadc2b5b31a4c
CCC <rustbell@hotmail.com>
2021-05-18 15:44:13 +0800

    panel sound

--- game/game/skin/skin_lr2_button_callbacks.cpp
+++ game/game/skin/skin_lr2_button_callbacks.cpp
@@ -6,6 +6,9 @@
 #include "game/data/option.h"
 #include "game/data/switch.h"
 
+#include "game/sound/sound_mgr.h"
+#include "game/sound/sound_sample.h"
+
 namespace lr2skin::button
 {
 
@@ -18,18 +21,18 @@ void panel_switch(int idx)
     if (gSwitches.get(panel))
     {
         // close panel
-        // TODO play sound
         gSwitches.set(panel, false);
         gTimers.set(static_cast<eTimer>(int(eTimer::PANEL1_START) - 1 + idx), -1);
         gTimers.set(static_cast<eTimer>(int(eTimer::PANEL1_END) - 1 + idx), t.norm());
+        SoundMgr::playSample(static_cast<size_t>(eSoundSample::SOUND_O_CLOSE));
     }
     else
     {
         // open panel
-        // TODO play sound
         gSwitches.set(panel, true);
         gTimers.set(static_cast<eTimer>(int(eTimer::PANEL1_START) - 1 + idx), t.norm());
         gTimers.set(static_cast<eTimer>(int(eTimer::PANEL1_END) - 1 + idx), -1);
+        SoundMgr::playSample(static_cast<size_t>(eSoundSample::SOUND_O_OPEN));
     }
 }
 
