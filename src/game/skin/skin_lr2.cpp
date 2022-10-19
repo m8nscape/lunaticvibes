c57efc1575e0b55df0e735ef83778648bdb2a370
rustbell S <rustbell@hotmail.com>
2022-10-19 13:47:49 +0800

    no

--- src/game/skin/skin_lr2.cpp
+++ src/game/skin/skin_lr2.cpp
@@ -765,12 +765,6 @@ int SkinLR2::IMAGE()
         }
         else
         {
-            // find *.png if fails
-            if (!fs::is_regular_file(pathFile) && !strEqual(pathFile.extension().u8string(), ".png", true))
-            {
-                std::string pathTmp = pathFile.u8string();
-                pathFile = PathFromUTF8(pathTmp.replace(pathTmp.length() - 4, 4, ".png"));
-            }
             Image img = Image(pathFile.u8string().c_str());
             if (info.hasTransparentColor)
                 img.setTransparentColorRGB(info.transparentColor);
