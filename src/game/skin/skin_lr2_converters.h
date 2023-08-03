e3c8c2e6a791b519af54b14d3a802ccc32f818e1
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2023-08-03 19:19:21 +0300

    Fix invalid use of preprocessor macros
    MSVC accepts this for some weird reason, but other compilers don't.

--- src/game/skin/skin_lr2_converters.h
+++ src/game/skin/skin_lr2_converters.h
@@ -14,17 +14,20 @@ std::function<int()> convertNumberIndex(int n);
 std::function<Ratio()> convertSliderIndex(int n);
 std::function<std::string_view()> convertTextIndex(int n);
 
-#define declare_member(converter, type, prefix, index)                                              \
-class _##prefix##_##index                                                                           \
-{                                                                                                   \
-public:                                                                                             \
-    template <typename U = converter, typename T = std::decay_t<decltype(U::##prefix##_##index)>>   \
-        static constexpr type(*value(U*, T* = nullptr))() { return &U::##prefix##_##index; }        \
-    template <typename U = converter>                                                               \
-        static constexpr type(*value(...))() { return &U::##prefix##_0; }                           \
+#define DECLARE_MEMBER_CONCAT_(a, b) a##b
+#define DECLARE_MEMBER_CONCAT(a, b) DECLARE_MEMBER_CONCAT_(a, b)
+
+#define declare_member(converter, type, prefix, index)                                                                                           \
+class DECLARE_MEMBER_CONCAT(_skin_conv_for_index_, index)                                                                                                       \
+{                                                                                                                                                \
+public:                                                                                                                                          \
+    template <typename U = converter, typename T = std::decay_t<decltype(U:: DECLARE_MEMBER_CONCAT(DECLARE_MEMBER_CONCAT(prefix, _), index))>>   \
+        static constexpr type(*value(U*, T* = nullptr))() { return &U:: DECLARE_MEMBER_CONCAT(DECLARE_MEMBER_CONCAT(prefix, _), index); }        \
+    template <typename U = converter>                                                                                                            \
+        static constexpr type(*value(...))() { return &U:: DECLARE_MEMBER_CONCAT(prefix, _0); }                                                  \
 };
 
-#define member(converter, type, prefix, index) _##prefix##_##index::value((converter*)0)
+#define member(converter, type, prefix, index) DECLARE_MEMBER_CONCAT(_skin_conv_for_index_, index)::value((converter*)0)
 
 }
 }
